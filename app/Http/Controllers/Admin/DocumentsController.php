546fcb1d841bf771da069e676ecff03a563e3fb0
vavicheth <vicheth_it@yahoo.com>
2020-04-20 15:53:21 +0700

    Upload with binary64

--- app/Http/Controllers/Admin/DocumentsController.php
+++ app/Http/Controllers/Admin/DocumentsController.php
@@ -27,26 +27,26 @@ class DocumentsController extends Controller
      */
     public function index()
     {
-        if (! Gate::allows('document_access')) {
+        if (!Gate::allows('document_access')) {
             return abort(401);
         }
 
-        $documents=Document::orderBy('created_at','DESC');
+        $documents = Document::orderBy('created_at', 'DESC');
 
         if (request('show_deleted') == 1) {
-            if (! Gate::allows('document_delete')) {
+            if (!Gate::allows('document_delete')) {
                 return abort(401);
             }
             $documents = $documents->onlyTrashed()->get();
         } elseif (request('show_all') == 1) {
             $documents = $documents->get();
-        } elseif (request('show_done') == 1)  {
-            $documents =$documents->whereSubmit(1)->get();
+        } elseif (request('show_done') == 1) {
+            $documents = $documents->whereSubmit(1)->get();
         } else {
-            $documents = $documents->where('submit','<>',1)->get();
+            $documents = $documents->where('submit', '<>', 1)->get();
         }
 
-        return view('admin.documents.index',compact('documents'));
+        return view('admin.documents.index', compact('documents'));
     }
 
     /**
@@ -56,26 +56,26 @@ class DocumentsController extends Controller
      */
     public function create()
     {
-        if (! Gate::allows('document_create')) {
+        if (!Gate::allows('document_create')) {
             return abort(401);
         }
 
         $user_createds = \App\User::get()->pluck('name', 'id')->prepend(trans('quickadmin.qa_please_select'), '');
         $user_updateds = \App\User::get()->pluck('name', 'id')->prepend(trans('quickadmin.qa_please_select'), '');
-        $users=User::orderBy('name','asc')->pluck('name','id')->all();
+        $users = User::orderBy('name', 'asc')->pluck('name', 'id')->all();
 
-        return view('admin.documents.create', compact('users','user_createds', 'user_updateds'));
+        return view('admin.documents.create', compact('users', 'user_createds', 'user_updateds'));
     }
 
     /**
      * Store a newly created Document in storage.
      *
-     * @param  \App\Http\Requests\StoreDocumentsRequest  $request
+     * @param \App\Http\Requests\StoreDocumentsRequest $request
      * @return \Illuminate\Http\Response
      */
     public function store(StoreDocumentsRequest $request)
     {
-        if (! Gate::allows('document_create')) {
+        if (!Gate::allows('document_create')) {
             return abort(401);
         }
 
@@ -86,8 +86,8 @@ class DocumentsController extends Controller
 
 
         foreach ($request->input('file_id', []) as $index => $id) {
-            $model          = config('medialibrary.media_model');
-            $file           = $model::find($id);
+            $model = config('medialibrary.media_model');
+            $file = $model::find($id);
             $file->model_id = $document->id;
             $file->save();
         }
@@ -101,12 +101,12 @@ class DocumentsController extends Controller
     /**
      * Show the form for editing Document.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
-        if (! Gate::allows('document_edit')) {
+        if (!Gate::allows('document_edit')) {
             return abort(401);
         }
 
@@ -114,21 +114,21 @@ class DocumentsController extends Controller
         $user_updateds = \App\User::get()->pluck('name', 'id')->prepend(trans('quickadmin.qa_please_select'), '');
 
         $document = Document::findOrFail($id);
-        $users=User::orderBy('name','asc')->get();
+        $users = User::orderBy('name', 'asc')->get();
 
-        return view('admin.documents.edit', compact('document', 'users','user_createds', 'user_updateds'));
+        return view('admin.documents.edit', compact('document', 'users', 'user_createds', 'user_updateds'));
     }
 
     /**
      * Update Document in storage.
      *
-     * @param  \App\Http\Requests\UpdateDocumentsRequest  $request
-     * @param  int  $id
+     * @param \App\Http\Requests\UpdateDocumentsRequest $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(UpdateDocumentsRequest $request, $id)
     {
-        if (! Gate::allows('document_edit')) {
+        if (!Gate::allows('document_edit')) {
             return abort(401);
         }
 
@@ -139,13 +139,13 @@ class DocumentsController extends Controller
 
         // Submit document and submit all comments in document for can not edit
         if ($request->get('submit') == 1) {
-            $document->comments()->update(['submit'=>'1']);
+            $document->comments()->update(['submit' => '1']);
         }
 
         $media = [];
         foreach ($request->input('file_id', []) as $index => $id) {
-            $model          = config('medialibrary.media_model');
-            $file           = $model::find($id);
+            $model = config('medialibrary.media_model');
+            $file = $model::find($id);
             $file->model_id = $document->id;
             $file->save();
             $media[] = $file->toArray();
@@ -161,32 +161,32 @@ class DocumentsController extends Controller
     /**
      * Display Document.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        if (! Gate::allows('document_view')) {
+        if (!Gate::allows('document_view')) {
             return abort(401);
         }
 
         $comments = \App\Comment::where('document_id', $id)->get();
         $document = Document::findOrFail($id);
-        $user_login=Auth::user();
+        $user_login = Auth::user();
 
-        return view('admin.documents.show', compact('document', 'comments','user_login'));
+        return view('admin.documents.show', compact('document', 'comments', 'user_login'));
     }
 
 
     /**
      * Remove Document from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        if (! Gate::allows('document_delete')) {
+        if (!Gate::allows('document_delete')) {
             return abort(401);
         }
         $document = Document::findOrFail($id);
@@ -202,7 +202,7 @@ class DocumentsController extends Controller
      */
     public function massDestroy(Request $request)
     {
-        if (! Gate::allows('document_delete')) {
+        if (!Gate::allows('document_delete')) {
             return abort(401);
         }
         if ($request->input('ids')) {
@@ -218,12 +218,12 @@ class DocumentsController extends Controller
     /**
      * Restore Document from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function restore($id)
     {
-        if (! Gate::allows('document_delete')) {
+        if (!Gate::allows('document_delete')) {
             return abort(401);
         }
         $document = Document::onlyTrashed()->findOrFail($id);
@@ -235,12 +235,12 @@ class DocumentsController extends Controller
     /**
      * Permanently delete Document from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function perma_del($id)
     {
-        if (! Gate::allows('document_delete')) {
+        if (!Gate::allows('document_delete')) {
             return abort(401);
         }
         $document = Document::onlyTrashed()->findOrFail($id);
@@ -251,23 +251,23 @@ class DocumentsController extends Controller
 
     public function index_user()
     {
-        if (! Gate::allows('document_access')) {
+        if (!Gate::allows('document_access')) {
             return abort(401);
         }
-        $user=Auth::user();
-        $documents=$user->document()->orderBy('created_at', 'DESC');
+        $user = Auth::user();
+        $documents = $user->document()->orderBy('created_at', 'DESC');
 
         if (request('show_deleted') == 1) {
-            if (! Gate::allows('document_delete')) {
+            if (!Gate::allows('document_delete')) {
                 return abort(401);
             }
             $documents = $documents->onlyTrashed()->get();
         } elseif (request('show_all') == 1) {
             $documents = $documents->get();
-        } elseif (request('show_done') == 1)  {
-            $documents =$documents->whereSubmit(1)->get();
+        } elseif (request('show_done') == 1) {
+            $documents = $documents->whereSubmit(1)->get();
         } else {
-            $documents = $documents->where('submit','<>',1)->get();
+            $documents = $documents->where('submit', '<>', 1)->get();
         }
 
         return view('admin.documents.index_user', compact('documents'));
@@ -283,7 +283,7 @@ class DocumentsController extends Controller
 //
 //        return view('admin.documents.print',compact('documents','users','docphotos','comments'));
 
-        if (! Gate::allows('document_view')) {
+        if (!Gate::allows('document_view')) {
             return abort(401);
         }
 
@@ -293,40 +293,39 @@ class DocumentsController extends Controller
         $comments = \App\Comment::where('document_id', $id)->get();
 
         $document = Document::findOrFail($id);
-        $user_login=Auth::user();
+        $user_login = Auth::user();
 
-        return view('admin.documents.print', compact('document', 'comments','user_login'));
+        return view('admin.documents.print', compact('document', 'comments', 'user_login'));
     }
 
-    public function view_pdf($document_id,$media_id)
+    public function view_pdf($document_id, $media_id)
     {
-        if (! Gate::allows('edit_pdf')) {
+        if (!Gate::allows('edit_pdf')) {
             return abort(401);
         }
         $document = Document::findOrFail($document_id);
-        $media=Media::findOrFail($media_id);
-        $file_name=asset('storage').'/' .$media->id.'/'.$media->file_name;
+        $media = Media::findOrFail($media_id);
+        $file_name = asset('storage') . '/' . $media->id . '/' . $media->file_name;
 
-        return view('admin.documents.view_pdf', compact('document','media','file_name'));
+        return view('admin.documents.view_pdf', compact('document', 'media', 'file_name'));
     }
 
     public function save_pdf(Request $request)
     {
-        if (! Gate::allows('edit_pdf')) {
+        if (!Gate::allows('edit_pdf')) {
             return abort(401);
         }
 
 //        return response($request->all());
 
-        if($request->hasFile('pdf'))
-        {
-//            $base64img = str_replace('data:application/pdf;base64,', '', $request['pdf']);
-//            $data = base64_decode($base64img);
+        if ($request->has('pdf')) {
+            $base64img = str_replace('data:application/pdf;base64,', '', $request['pdf']);
+            $data = base64_decode($base64img);
 //
 //            Storage::put($request->media_id.'/'.$request->media_file, $data);
 
 
-//            Storage::disk("public")->put($request->media_id.'/'.$request->media_file, $data);
+            Storage::disk("public")->put($request->media_id.'/'.$request->media_file, $data);
 
 
 //            $base64_image = $request->input('pdf'); // your base64 encoded
@@ -336,25 +335,41 @@ class DocumentsController extends Controller
 //            Storage::disk('public')->put($imageName, base64_decode($file_data));
 
 
-
 //            $file = $request->media_id.'/'.$request->media_file;
 //            file_put_contents($file, $data);
 
 //            Storage::put($request->media_id.'/'.$request->media_file, (string) file_get_contents($data), 'public');
 
-
-            $file = $request->file('pdf');
-            Storage::put($request->media_id.'/'.$request->media_file, (string) file_get_contents($file), 'public');
-
             //Show approved when DG comment
-            if (auth()->id() == 28)
-            {
-                $document=Document::findOrFail($request->document_id);
+            if (auth()->id() == 28) {
+                $document = Document::findOrFail($request->document_id);
                 $document->update(['submit' => '2']);
             }
 
-            return response('Successful!');
+            return response('Successful binary save to file!');
         }
+
+
+        /**
+         * For Blob object
+         */
+
+        if ($request->hasFile('pdf')) {
+
+            $file = $request->file('pdf');
+            Storage::put($request->media_id . '/' . $request->media_file, (string)file_get_contents($file), 'public');
+
+            //Show approved when DG comment
+            if (auth()->id() == 28) {
+                $document = Document::findOrFail($request->document_id);
+                $document->update(['submit' => '2']);
+            }
+
+            return response('Successful blog save to file!');
+        }
+
+
+
 
         return response('File can not edit!');
     }
